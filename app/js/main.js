c2336b2facb970ac19dfdf2cb5416772f2eb78c3
Le8wa-dev <savinchev@list.ru>
2019-11-14 17:19:48 +0300

    finish static

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 $(function(){
 
+    // отображение меню, путем добавления класса showed
     $('.header__menu-btn').on('click', function(){
         $('.header__menu-list').toggleClass('showed');
     });
 
+    // плагин Rangeslider для изменения величины площади(м2)
     $(".js-range-slider").ionRangeSlider({
         min: 0,
         max: 250000,
@@ -11,6 +13,22 @@ $(function(){
         postfix: " м2",
     });
 
+    // Плавный скролл к якорным ссылкам
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 
+    // скрипт для кнопки возврата вверх страницы
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+        $('.scrollup').fadeIn();
+        } else {
+        $('.scrollup').fadeOut();
+        }
+    }); 
+
+    // плагин Mixitup для сортировки элементов портфолио
     var mixer = mixitup('.portfolio__inner-items');
 });
